0f317b04c07a9a35c48f240709bf61dd15c44185
Noah Burton <nburton115@gmail.com>
2023-07-31 23:09:49 -0600

    checkboxes logic, rm add btn, rm list item

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,19 +19,31 @@ function showDates() {
 // API Key
 var apiKey = '9dNeLq2nvcwlctGtvDnExgSlMHam78mtKPhgrnn9';
 
+// ref to the entire CAMPGROUND LIST container
+var campContainer = document.getElementById('camp-container');
+
 // var for user checkbox: "do you have a campsite picked out yet?"
 //var needCampHelp = 'false';
 
 // --CAMPGROUND API PARAMETERS--
 var campResultsLimit = 25;
 // TO DO: Get park code from user response
+//var parkName = document.getElementById("park-input").value;
 var parkCode = 'care';
+
+
+function clearCampContent() {
+  var campContentEl = document.getElementById('camp-list');
+  campContentEl.innerHTML = '';
+}
 
 
 // TO DO: add conditional statement based on checkbox for camp help
 
-function campSelect() {
-  // TO DO: clear form content before generating list
+function generateCampList(parkCode, apiKey) {
+  // clear content if nes
+  clearCampContent();
+  
 
  var campCall = 'https://developer.nps.gov/api/v1/campgrounds?parkCode=' + parkCode + '&api_key=' + apiKey;
  //console.log(campCall);
@@ -46,8 +58,52 @@ function campSelect() {
  })
  .then(function(data){
   console.log(data);
+  // --CAMP LISTINGS--
+  //for (let i = 0; i < array.length; i++) {
+    
+    // camp name
+    //var makeCampHeading = document.createElement('h5');
+    
+    // bathroom
+
+    //water
+
+    // trash
+
+    // button to show details
+  
+ // }
+  
  })
 }
 
-// TODO: remove this function call for user checkbox choice.
-campSelect();
+// Check boxes that display/hide CAMPGROUND LIST div element
+var findHelpCheckbox = document.getElementById('find-help');
+var findOnMyOwnCheckbox = document.getElementById('find-on-my-own');
+
+findHelpCheckbox.addEventListener('change', function (event) { // chat gpt assisted with these two event listeners
+  if (event.target.checked) {
+      // "I need help finding a campsite" checkbox is checked
+      if (findOnMyOwnCheckbox.checked) {
+          findOnMyOwnCheckbox.checked = false; // Uncheck the other checkbox
+      }
+      campContainer.style.display = 'block';
+      generateCampList(parkCode, apiKey);
+  } else {
+      // "I need help finding a campsite" checkbox is unchecked
+      
+  }
+});
+
+findOnMyOwnCheckbox.addEventListener('change', function (event) {
+  if (event.target.checked) {
+      // "I will find it on my own" checkbox is checked
+      if (findHelpCheckbox.checked) {
+          findHelpCheckbox.checked = false; // Uncheck the other checkbox
+      }
+      campContainer.style.display = 'none';
+  } else {
+      // "I will find it on my own" checkbox is unchecked
+      
+  }
+});
