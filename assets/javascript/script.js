c2ebc694844a19a813f73d4202732cd92ac17c1d
Noah Burton <nburton115@gmail.com>
2023-07-31 20:11:32 -0600

    set up api call, parameters, pseudo-code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,6 +16,38 @@ function showDates() {
 }
 
 
-// var API Key
+// API Key
 var apiKey = '9dNeLq2nvcwlctGtvDnExgSlMHam78mtKPhgrnn9';
-// var for user checkbox: "do you have a campsite picked out yet"
+
+// var for user checkbox: "do you have a campsite picked out yet?"
+//var needCampHelp = 'false';
+
+// --CAMPGROUND API PARAMETERS--
+var campResultsLimit = 25;
+// TO DO: Get park code from user response
+var parkCode = 'care';
+
+
+// TO DO: add conditional statement based on checkbox for camp help
+
+function campSelect() {
+  // TO DO: clear form content before generating list
+
+ var campCall = 'https://developer.nps.gov/api/v1/campgrounds?parkCode=' + parkCode + '&api_key=' + apiKey;
+ //console.log(campCall);
+ fetch(campCall)
+ .then(function(response){
+  if (!response.ok) {
+    // TODO: we can't use alert windows. if something goes wrong with call we need another way to display it.
+    console.log('api error');
+  } else {
+    return response.json();                       
+  }
+ })
+ .then(function(data){
+  console.log(data);
+ })
+}
+
+// TODO: remove this function call for user checkbox choice.
+campSelect();
