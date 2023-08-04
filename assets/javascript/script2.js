cbb1df32d61bd3539a862f1ae0c07ce78446d45d
Noah Burton <nburton115@gmail.com>
2023-08-04 14:22:20 -0600

    toggle to display campsite details

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -2,15 +2,38 @@ $("#back-btn").on('click', function() {
     window.location.replace('./index.html');
 })
 
-function getMyCampSiteData () {
+var myCampContentEl = document.getElementById('my-camp-content');
 
+// var for if camp site data is visible
+var myCampVisible = false; // not visible by default
+function displayDetailsEl() {
+    
+    if (myCampVisible === true) {
+        myCampContentEl.style.display = 'block'; 
+        console.log('details on');
+    } else {
+        myCampContentEl.style.display = 'none';
+        console.log('details off');
+    }
+}
+window.onload = displayDetailsEl;
+// while (myCampVisible === false) {
+//     myCampContentEl.style.display = 'none';
+//     console.log('details off')
+// }
+function getMyCampSiteData () {
+    console.log('getMyCampSiteData is exe');
     var savedCampsiteData = JSON.parse(localStorage.getItem('My-Site'));
     
     console.log(savedCampsiteData);
-   
-    return savedCampsiteData
+    
+
+    // toggle visibility when View My Site is clicked
+    myCampVisible = !myCampVisible; 
+    
+    // return savedCampsiteData
 }
 
-//  to do: get ref to camp name for above function
-
+// event listener for 'view my site' button
 $('#view-my-site').on('click', getMyCampSiteData);
+$('#view-my-site').on('click', displayDetailsEl);
