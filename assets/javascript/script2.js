f29f70a8ee8c45f4dce6c1743431591392b8a9e9
Noah Burton <nburton115@gmail.com>
2023-08-04 15:35:31 -0600

    details populated in view my campsite

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -31,7 +31,73 @@ function getMyCampSiteData () {
     // toggle visibility when View My Site is clicked
     myCampVisible = !myCampVisible; 
     
-    // return savedCampsiteData
+    // generate info
+
+    //trash
+    var trashNotesEl = document.getElementById('trash-notes');
+    var trashNotes = savedCampsiteData.amenities.trashRecyclingCollection;
+    if (trashNotes === '') {
+        trashNotesEl.textContent = 'None';
+
+      } else {
+        trashNotesEl.textContent = trashNotes;
+      }
+    
+    // toilets
+    var toiletsEl = document.getElementById('toilet-notes');
+    var toiletNotes = savedCampsiteData.amenities.toilets[0];
+    toiletsEl.textContent = toiletNotes;
+    if (toiletsEl.textContent === '') {
+            toiletsEl.textContent = 'None';
+    }
+    
+    // cell reception
+    var receptionEl = document.getElementById('reception-notes');
+    var receptionNotes = savedCampsiteData.amenities.cellPhoneReception;
+    receptionEl.textContent = receptionNotes;
+    if (receptionEl.textContent === '') {
+      receptionEl.textContent = 'No information available';
+    }      
+
+    // host availability
+    var hostEl = document.getElementById('host-notes');
+    var hostNotes = savedCampsiteData.amenities.staffOrVolunteerHostOnSite;
+    hostEl.textContent = hostNotes;
+    if (hostEl.textContent === '') {
+      hostEl.textContent = 'No information available';
+    } 
+
+    // camp description
+    var campDescriptionEl = document.getElementById('camp-info-text');
+    var campNotes = savedCampsiteData.campDescription;
+    campDescriptionEl.textContent = campNotes;
+    if (campDescriptionEl.textContent === '') {
+      campDescriptionEl.textContent = 'Sorry, no reservation information is available for this campsite.'
+    }
+
+    // camp name on details page
+    var campNameSubHead = document.getElementById('camp-detail-name');
+    var campTitle = savedCampsiteData.campTitle;
+     campNameSubHead.textContent = campTitle;
+
+    // reservation info text below description
+    var campReservationInfoEl = document.getElementById('reservation-notes');
+    var campResNotes = savedCampsiteData.campResNotes;
+    campReservationInfoEl.textContent = campResNotes; 
+
+    // regulations
+    var campRegulationsEl = document.getElementById('camp-regulations-text');
+    var regulationNotes = savedCampsiteData.regulationNotes;
+    campRegulationsEl.textContent = regulationNotes;
+    
+    var campRegUrlEl = document.getElementById('regulations-url');
+    var campRegUrl = savedCampsiteData.campRegUrl;
+    campRegUrlEl.textContent = campRegUrl;
+    if (campRegulationsEl.textContent === '' && campRegUrlEl.textContent === ''){
+        campRegulationsEl.textContent = 'No regulation information available.'
+    }
+
+    
 }
 
 // event listener for 'view my site' button
